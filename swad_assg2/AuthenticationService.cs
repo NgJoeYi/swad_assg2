65cd4be3a35858bd350a9eee3915b69c9147feba
goameliagoh <s10257856@connect.np.edu.sg>
2024-08-03 19:22:23 +0800

    (AG) Code done

---
+++ swad_assg2/AuthenticationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swad_assg2
+{
+    class AuthenticationService
+    {
+        public bool PerformAuthenticityCheck(Renter renter)
+        {
+            if (CalculateAge(renter.DateOfBirth) <= 17)
+            {
+                Console.WriteLine("Authentication failed: Renter must be at least 18 years old.");
+                return false;
+            }
+
+            // Additional authentication checks can be added here
+            return true;
+        }
+
+        private int CalculateAge(DateTime birthDate)
+        {
+            int age = DateTime.Now.Year - birthDate.Year;
+            if (DateTime.Now.DayOfYear < birthDate.DayOfYear)
+                age--;
+            return age;
+        }
+    }
+}
---
+++ swad_assg2/Renter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swad_assg2
+{
+    class Renter
+    {
+        public string FullName { get; set; }
+        public string ContactDetails { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string DriversLicense { get; set; }
+        public string InsuranceDetails { get; set; }
+
+        public Renter(string fullName, string contactDetails, DateTime dateOfBirth, string driversLicense, string insuranceDetails)
+        {
+            FullName = fullName;
+            ContactDetails = contactDetails;
+            DateOfBirth = dateOfBirth;
+            DriversLicense = driversLicense;
+            InsuranceDetails = insuranceDetails;
+        }
+    }
+}
