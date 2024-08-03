f2bd22d0b0c04aca2eb58fabeb2036c0e0fbdf6e
LEE WEI YING <148225902+emery97@users.noreply.github.com>
2024-08-03 21:42:50 +0800

    added admin

---
+++ swad_assg2/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swad_assg2
+{
+    internal class Admin
+    {
+    }
+}
